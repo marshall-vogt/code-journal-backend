f9e5d9e7b4491a099568e61a90242ade3bdb2ef2
Marshall Vogt <vogt_marshall@yahoo.com>
2023-09-20 17:01:59 +0000

    Added get method to server.js

--- server/server.js
+++ server/server.js
@@ -12,6 +12,21 @@ const db = new pg.Pool({
 });
 
 const app = express();
+app.use(express.json());
+
+app.get('/api/entries', async (req, res, next) => {
+  try {
+    const sql = `
+      select *
+        from "entries"
+        order by "entryId" desc
+    `;
+    const result = await db.query(sql);
+    res.json(result.rows);
+  } catch (err) {
+    next(err);
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`express server listening on port ${process.env.PORT}`);
